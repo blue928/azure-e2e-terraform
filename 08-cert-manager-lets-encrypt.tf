7b838cf8fd77c15f17142358434b977a67a25d5c
Blue Presley <bluepresley@gastonia.com>
2022-04-05 09:16:23 -0400

    upgrade cert-manager chart version to 1.7.2

---
+++ 08-cert-manager-lets-encrypt.tf
@@ -0,0 +1,22 @@
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.7.2"
+
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  #values = [file("cert-manager-values.yaml")]
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [
+    module.aks-cluster,
+    module.aks-cluster-required-config,
+  ]
+
+}
