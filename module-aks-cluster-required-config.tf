189089a272ed38fe83ddcb0a02e0bd99fffa7063
Blue Presley <bluepresley@gastonia.com>
2024-03-26 22:16:38 -0400

    comment out ref to custom ip

--- module-aks-cluster-required-config.tf
+++ module-aks-cluster-required-config.tf
@@ -9,7 +9,7 @@ module "aks-cluster-required-config" {
   #source                         = "github.com/blue928/aks-cluster-required-configuration-terraform-module"
   source                         = "git::git@github.com:blue928/terraform-aks-cluster-required-configuration-module.git"
   storage_class_cluster_location = module.azure-rg.resource_group_location # Must be the same for the cluster module. 
-  lb_public_ip                   = module.aks-cluster.lb_public_ip
+  #lb_public_ip                   = module.aks-cluster.lb_public_ip
   #lets encrypt
   #ingress class
 }
--- module-azure-dns-config.tf
+++ module-azure-dns-config.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "azure-dns" {
-  source = "git::git@github.com:blue928/terraform-azurerm-dns-module.git"
+  source              = "git::git@github.com:blue928/terraform-azurerm-dns-module.git"
   azure_dns_zone      = local.azure_dns_zone
   resource_group_name = module.azure-rg.resource_group_name
 }
